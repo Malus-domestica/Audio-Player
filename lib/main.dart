3fac9e8bcd5e0461b1a6ea324e693cba6aed46e2
Malus-domestica <fshafique57@gmail.com>
2022-08-21 19:12:53 +0530

    modified main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Audio Reading',
+      title: 'FlutteR Audio Reading',
       debugShowCheckedModeBanner: false,
       home: MindfulnessScreen(),
       // home: SreenDrawer(),
